a6469ec252c8a5f766482057ff59eebd88ea733a
coderpratik777 <coderpratik321@gmail.com>
2023-02-24 16:37:32 +0530

    Added controller code for hello.jsp

--- src/main/java/com/pratiti/controller/HelloController.java
+++ src/main/java/com/pratiti/controller/HelloController.java
@@ -1,5 +1,17 @@
 package com.pratiti.controller;
 
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class HelloController {
+	
+	@RequestMapping("/hello")
+	public String hello(Map map) {
+		map.put("message","Welcome to spring MVC");
+		return "hello.jsp";
+	}
 
 }
